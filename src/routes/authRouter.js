477701ef716bb06e483c7f027e12e09e35db04a3
Dener Silva <dener.ufes@gmail.com>
2022-08-03 15:05:07 -0300

    Criada a rota de cadastro

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { signUp } from "../controllers/authController.js";
 
 const authRouter = Router();
 
-authRouter.post("/", );
+authRouter.post("/signup", signUp);
 authRouter.post("/", );
 
 export default authRouter;
