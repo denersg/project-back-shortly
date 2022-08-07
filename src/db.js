e394d20c939755093b26e41288e8dad66aae8ad9
Dener Silva <dener.ufes@gmail.com>
2022-08-06 23:50:24 -0300

    Projeto Shortly finalizado

--- src/db.js
+++ src/db.js
@@ -4,22 +4,14 @@ dotenv.config();
 
 const { Pool } = pg;
 
-// const config = {
-//     connectionString: process.env.DATABASE_URL
-// }
+const config = {
+    connectionString: process.env.DATABASE_URL
+}
 
 if(process.env.MODE === "PROD"){
     config.ssl = {
         rejectUnauthorized: false
     }
-}
-
-let config = {
-    user: "postgres",
-    password: "",
-    host: "localhost",
-    port: 5432,
-    database: "shortly_database"
 }
 
 const db = new Pool(config);
