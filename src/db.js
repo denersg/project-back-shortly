04a774c64df7daac441afb6c44616516a1bd5d7e
Dener Silva <dener.ufes@gmail.com>
2022-08-12 08:53:55 -0300

    Database problems resolved

--- src/db.js
+++ src/db.js
@@ -4,16 +4,11 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const config = {
-    connectionString: process.env.DATABASE_URL
-}
-
-if(process.env.MODE === "PROD"){
-    config.ssl = {
+const db = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
         rejectUnauthorized: false
     }
-}
-
-const db = new Pool(config);
+});
 
 export default db;
